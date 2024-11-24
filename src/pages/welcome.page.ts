7662347009156473d8061d40a3958dfe2a94e52a
Magdalena Tyszkiewicz-Konopka <magdalena.tyszkiewicz-konopka@capgemini.com>
2024-11-24 13:19:53 +0100

    test: login scenario

---
+++ src/pages/welcome.page.ts
@@ -0,0 +1,9 @@
+import { BasePage } from './base.page';
+import { Page } from '@playwright/test';
+
+export class WelcomePage extends BasePage {
+  url = '/welcome';
+  constructor(page: Page) {
+    super(page);
+  }
+}
