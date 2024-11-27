0eca8ad2fd9a008f6d93bc64984acc90c1629170
Magdalena <tyszkiewicz.konopqa@gmail.com>
2024-11-27 21:54:27 +0100

    test: login invalid password

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -3,13 +3,19 @@ import { Page } from '@playwright/test';
 
 export class LoginPage extends BasePage {
   url = '/login';
+  userEmail = this.page.getByPlaceholder('Enter User Email');
+  userPassword = this.page.getByPlaceholder('Enter Password');
+  loginButton = this.page.getByRole('button', { name: 'LogIn' });
+
+  loginError = this.page.getByTestId('login-error');
+
   constructor(page: Page) {
     super(page);
   }
 
   async login(email: string, password: string): Promise<void> {
-    await this.page.getByPlaceholder('Enter User Email').fill(email);
-    await this.page.getByPlaceholder('Enter Password').fill(password);
-    await this.page.getByRole('button', { name: 'LogIn' }).click();
+    await this.userEmail.fill(email);
+    await this.userPassword.fill(password);
+    await this.loginButton.click();
   }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,7 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
 import { testUser1 } from '../src/test-data/user.data';
-import { expect, test } from '@playwright/test';
+import test, { expect } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @GAD_R02_01', async ({ page }) => {
@@ -18,6 +18,21 @@ test.describe('Verify login', () => {
     const title = await welcomePage.title();
 
     //Assert
-    expect(title).toContain('Welcome');
+    expect(title).toContain('Login');
+  });
+
+  test('reject login with incorrect password @GAD_R02_01', async ({ page }) => {
+    //Arrange
+    const userEmail = testUser1.userEmail;
+    const userPassword = 'incorrectPassword';
+    const loginPage = new LoginPage(page);
+
+    //Act
+    await loginPage.goto();
+    await loginPage.login(userEmail, userPassword);
+
+    //Assert
+    const errorMessage = loginPage.loginError;
+    await expect(errorMessage).toContainText('Invalid username or password');
   });
 });
