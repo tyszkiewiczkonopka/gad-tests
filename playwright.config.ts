85af5bde51e232f852f593cffa880aca7a128c6e
Magdalena Tyszkiewicz-Konopka <magdalena.tyszkiewicz-konopka@capgemini.com>
2024-11-27 14:16:55 +0100

    feat: dotenv for env var

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,8 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
   testDir: './tests',
+  globalSetup: 'src/global-setup.ts',
   timeout: 60_000,
   expect: { timeout: 10_000 },
   fullyParallel: true,
@@ -12,7 +12,7 @@ export default defineConfig({
   workers: undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
     actionTimeout: 0,
     trace: 'on',
     video: 'retain-on-failure',
