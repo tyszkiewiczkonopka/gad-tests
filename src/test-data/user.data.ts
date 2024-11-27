6150a93d9a8a41635b8ccea5062acfcb1e5f6795
Magdalena Tyszkiewicz-Konopka <magdalena.tyszkiewicz-konopka@capgemini.com>
2024-11-27 17:52:58 +0100

    chore: test data for user password

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,3 +1,15 @@
 export const testUser1 = {
   userEmail: process.env.USER_EMAIL ?? '[NOT SET]',
+  userPassword: process.env.USER_PASSWORD ?? '[NOT SET]',
 };
+
+// export const getTestUser = () => {
+//   const userEmail = process.env.USER_EMAIL;
+//   const userPassword = process.env.USER_PASSWORD;
+
+//   if (!userEmail || !userPassword) {
+//     throw new Error('Missing user login data');
+//   }
+
+//   return { userEmail, userPassword };
+// };
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,7 +7,7 @@ test.describe('Verify login', () => {
   test('login with correct credentials @GAD_R02_01', async ({ page }) => {
     //Arrange
     const userEmail = testUser1.userEmail;
-    const userPassword = 'test1';
+    const userPassword = testUser1.userPassword;
     const loginPage = new LoginPage(page);
 
     //Act
