4e7d3e0c0d88f236ff4b2cd5679f12a9bac1436d
Magdalena <tyszkiewicz.konopqa@gmail.com>
2025-05-11 19:25:39 +0200

    refactor: added page fixtures to menu.spec

--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,47 +1,62 @@
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
-import { expect, test } from '@playwright/test';
+import { test as baseTest, expect } from '@playwright/test';
+
+interface Pages {
+  articlesPage: ArticlesPage;
+  commentsPage: CommentsPage;
+}
+
+const test = baseTest.extend<Pages>({
+  articlesPage: async ({ page }, use) => {
+    const articlesPage = new ArticlesPage(page);
+    await articlesPage.goto();
+    await use(articlesPage);
+  },
+  commentsPage: async ({ page }, use) => {
+    const commentsPage = new ArticlesPage(page);
+    await commentsPage.goto();
+    await use(commentsPage);
+  },
+});
 
 test.describe('Verify menu main buttons', () => {
   test('comments button navigates to comments page @GAD-R01-03', async ({
-    page,
+    articlesPage,
+    commentsPage,
   }) => {
     //Arrange
     const expectedCommentsTitle = 'Comments';
-    const articlesPage = new ArticlesPage(page);
 
     //Act
-    await articlesPage.goto();
-    const commentsPage = await articlesPage.mainMenu.clickCommentsButton();
+    await articlesPage.mainMenu.clickCommentsButton();
     const title = await commentsPage.getTitle();
 
     //Assert
     expect(title).toContain(expectedCommentsTitle);
   });
   test('articles button navigates to articles page @GAD-R01-03', async ({
-    page,
+    articlesPage,
+    commentsPage,
   }) => {
     //Arrange
     const expectedArticlesTitle = 'Articles';
-    const commentsPage = new CommentsPage(page);
 
     //Act
     await commentsPage.goto();
-    const articlesPage = await commentsPage.mainMenu.clickArticlesButton();
+    await commentsPage.mainMenu.clickArticlesButton();
     const title = await articlesPage.getTitle();
 
     //Assert
     expect(title).toContain(expectedArticlesTitle);
   });
   test('home page button navigates to main page @GAD-R01-03', async ({
-    page,
+    articlesPage,
   }) => {
     //Arrange
     const expectedHomePageTitle = 'GAD';
-    const articlesPage = new ArticlesPage(page);
 
     //Act
-    await articlesPage.goto();
     const homePage = await articlesPage.mainMenu.clickHomePageLink();
     const title = await homePage.getTitle();
 
